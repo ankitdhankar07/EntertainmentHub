ba5d8b58148f136337af162c38623829d4334cc7
ankitdhankar07 <ankitdhankar16@gmail.com>
2025-02-17 16:44:01 +0530

    added badge icon in card

--- src/components/SingleContent/SingleContent.jsx
+++ src/components/SingleContent/SingleContent.jsx
@@ -1,10 +1,13 @@
+import { Badge } from "@mui/material";
 import React from "react";
 import { img_300, unavailable } from "../../Config/Config";
 import "./SingleContent.css";
 
-function SingleContent({ id, title, poster, date, media_type }) {
+function SingleContent({ id, title, poster, date, media_type ,  vote_average  }) {
+  vote_average = Math.floor(vote_average);
   return (
     <div className="media">
+      <Badge color={vote_average>6?"primary":"secondary"} badgeContent={vote_average}/>
       <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
       <b className="title">{title} </b>
       <span className="subTitle">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,6 @@ import './Header.css'
 
 export default function Header() {
   return (
-    <div className='header'>Entertainment Hub</div>
+    <div className='header' onClick={()=>{window.scroll(0,0)}}>Entertainment Hub</div>
   )
 }
