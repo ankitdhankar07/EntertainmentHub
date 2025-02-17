aa484e2dc45fbfaa203387321ed6e7b263b71d71
ankitdhankar07 <ankitdhankar16@gmail.com>
2025-02-17 14:45:38 +0530

    created singleContent component and fetching data from the movie db api using axios

---
+++ src/components/SingleContent/SingleContent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SingleContent({id,title,poster,date,media_type}) {
+  return <div>
+    {title} 
+
+  </div>;
+}
+
+export default SingleContent;
--- src/pages/trending/Trending.jsx
+++ src/pages/trending/Trending.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 function Trending() {
+  const [content, setContent] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/trending/all/day",
+    params: { language: "en-US" },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmVjMWNjY2NhMzk4ZmExZTExZTQ4MjdmNWJjNDBiMiIsIm5iZiI6MTY1OTIwNzQ4NC4zMDgsInN1YiI6IjYyZTU3ZjNjODEzY2I2MDA1YTMxYTE3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rR-ut7EeNO7VhjxMaIKgbgEVbAqoykhpJ2QoMvkwhBY",
+    },
+  };
+
+  const fetchTrending = async () => {
+    let { data } = await axios.request(options);
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
   return (
-    <div><span className='pageTitle'>Tredning
-        </span></div>
-  )
+    <div>
+      <span className="pageTitle">Trending</span>
+      <SingleContent />
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              title={c.title || c.name}
+              poster={c.poster_path}
+              date={c.release_date || c.first_air_date}
+              media_type={c.media_type}
+              vote_average = {c.vote_average}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default Trending
+export default Trending;
