b499fdd9461ebbbeaa64f6be61aa8212d036d4c7
ankitdhankar07 <ankitdhankar16@gmail.com>
2025-02-18 14:43:30 +0530

    functions to add and remove the genres

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -33,9 +33,33 @@ function Genres({
     };
   }, []);
 
-  return <div style = {{padding:"6px 0"}}>
-        {genres.map((genre)=>(<Chip/>))}
-  </div>;
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
+  return (
+    <div style={{ padding: "6px 0" }}>
+      {genres.map((genre) => (
+        <Chip
+          label={genre.name}
+          style={{ margin: "2.5px" }}
+          clickable
+          size="small"
+          key={genre.id}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Genres;
