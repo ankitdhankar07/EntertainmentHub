23aa7da54b2ae355bc57a7ddea204b3b59c86011
ankitdhankar07 <ankitdhankar16@gmail.com>
2025-02-18 18:20:57 +0530

    created search component

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, Tab, Tabs, TextField } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import "./Search.css";
 
 function Search() {
+  const [type, setType] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/search/${
+      type ? "tv" : "movie"
+    }`,
+    params: {
+      query: searchText,
+      include_adult: "false",
+      language: "en-US",
+      page: page,
+    },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmVjMWNjY2NhMzk4ZmExZTExZTQ4MjdmNWJjNDBiMiIsIm5iZiI6MTY1OTIwNzQ4NC4zMDgsInN1YiI6IjYyZTU3ZjNjODEzY2I2MDA1YTMxYTE3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rR-ut7EeNO7VhjxMaIKgbgEVbAqoykhpJ2QoMvkwhBY",
+    },
+  };
+
+  const fetchSearch = async () => {
+    const { data } = await axios.request(options);
+    console.log(data.results);
+    setContent(data.results);
+    setNumOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchSearch();
+  }, [type, page]);
+
   return (
-    <div><span className='pageTitle'>search</span></div>
-  )
+    <div className="searchPage">
+      <div className="search">
+        <TextField
+          style={{ flex: 1 }}
+          className="searchBox"
+          label="Search"
+          variant="filled"
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <Button
+          onClick={fetchSearch}
+          variant="contained"
+          style={{ marginLeft: 10 }}
+        >
+          <SearchIcon fontSize="large" />
+        </Button>
+      </div>
+      
+      <Tabs
+          value={type}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(event, newValue) => {
+            setType(newValue);
+            setPage(1);
+          }}
+          style={{ paddingBottom: 5 }}
+          aria-label="disabled tabs example"
+        >
+          <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
+        </Tabs>
+        <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={type ? "tv" : "movie"}
+              vote_average={c.vote_average}
+            />
+          ))}
+        {searchText &&
+          !content &&
+          (type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
+    </div>
+  );
 }
 
-export default Search
+export default Search;
