bbf58d4ba55aef1e9ab674aaf06a3fb7dd7a23a8
ankitdhankar07 <ankitdhankar16@gmail.com>
2025-02-18 12:51:17 +0530

    added count in pagination

--- src/components/Pagination/CustomPagination.jsx
+++ src/components/Pagination/CustomPagination.jsx
@@ -1,16 +1,20 @@
-import { Pagination } from '@mui/material';
-import React from 'react';
+import { Pagination } from "@mui/material";
+import React from "react";
 
-function CustomPagination({setPage}) {
-    const handlePageChange=(page)=>{
-        setPage(page);
-        window.scroll(0,0);
-    }
+function CustomPagination({ setPage }) {
+  const handlePageChange = (page) => {
+    setPage(page);
+    window.scroll(0, 0);
+  };
   return (
     <div>
-        <Pagination count={10} onChange={(e)=>handlePageChange(e.target.textContent)}/>
+      <Pagination
+        count={10}
+        color="primary"
+        onChange={(e) => handlePageChange(e.target.textContent)}
+      />
     </div>
-  )
+  );
 }
 
-export default CustomPagination
+export default CustomPagination;
